6e008438e736eeedb1dd130e583bc7a8b0cc0217
mahesh <mail2mahebabu@gmail.com>
2025-06-15 23:03:24 +0530

    added new methods

--- src/match_operations.py
+++ src/match_operations.py
@@ -13,3 +13,6 @@ def add(a,b):
 
 def sub(a,b):
     return a-b
+
+def multiply(a,b):
+    return a*b
--- tests/test_operations.py
+++ tests/test_operations.py
@@ -1,3 +1,6 @@
-from src.match_operations import add, sub
+from src.match_operations import add, sub, multiply
 def test_check():
     assert add(2,3)==5
+
+def test_multiply():
+    assert multiply(2,3)==6
